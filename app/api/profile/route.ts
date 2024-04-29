39618a25e8dfa8a9b84e205b9be7b4634547e99a
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-04-29 10:29:57 +0530

    made better schema

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,7 +1,27 @@
-import { NextRequest } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { PrismaClient } from '@prisma/client'
 const prisma = new PrismaClient()
 export async function POST(req:NextRequest){
+    try {
     const data = await req.json()
-    const responce = await prisma.brand
+    const responce = await prisma.brand.findMany({
+        where:{
+            id:data.id,
+        },
+        include:{
+            _count:{
+                select:{
+                    products:true
+                }
+            }
+        }
+    })
+    return NextResponse.json({
+        responce
+    })
+    } catch (error) {
+        
+    }
+    
+
 }
