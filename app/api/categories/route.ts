7092319a4cffaee1e96940a047e37722959fbdbc
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-04-29 19:34:06 +0530

    added statics code

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -8,19 +8,23 @@ export function POST(){
             data:[
                 {
                     id:1,
-                    category:"Biryani"
+                    category:"Biryani",
+                    imgurl:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Baguettes%2C_Paris%2C_France_-_panoramio.jpg/1920px-Baguettes%2C_Paris%2C_France_-_panoramio.jpg"
                 },
                 {
                     id:2,
-                    category:"Curry"
+                    category:"Curry",
+                    imgurl:"https://static.wixstatic.com/media/91e241_475faa4fa56341f3878699dde5ab4904~mv2.jpg/v1/fill/w_864,h_576,al_c,lg_1,q_85,enc_auto/91e241_475faa4fa56341f3878699dde5ab4904~mv2.jpg"
                 },
                 {
                     id:3,
-                    category:"Breads"
+                    category:"Breads",
+                   imgurl:"https://openthemagazine.com/wp-content/uploads/2015/11/frogfood1.jpg"
                 },
                 {
                     id:4,
-                    category:"Chinese"
+                    category:"Chinese",
+                    imgurl:"https://images.herzindagi.info/image/2020/Oct/chole-bhature-calories-one-plate.jpg"
                 },
             ]
                 
--- components/productCard.tsx
+++ components/productCard.tsx
@@ -1,6 +1,8 @@
 export default function Productcard(props:any){
-    return <div className="h-60  w-64 border-2 border-gray-200  bg-white mx-5 rounded-sm shadow-sm flex-col justify-start items-start hover:scale-105 duration-100 cursor-pointer ">
-        <img src={props.url} className="h-44 w-64 rounded-t-sm"></img>
+    return <div className="h-60  w-64 border-2 border-gray-200  bg-white mx-5 rounded-sm shadow-sm flex-col justify-start items-start hover:scale-105 duration-150 cursor-pointer ">
+        <div className="flex  justify-center items-center h-44 bg-gray-200">
+        <img src={props.url} className="h-40 w-48  rounded-t-sm"></img>
+        </div>
         <div className="pl-4">{props.name}</div>
         <div className="flex justify-between items-center px-4" >
             <div>items in stock: {props.stock}</div>
