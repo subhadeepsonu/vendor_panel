7156149c67522761afcd52bd0fd422987e2d6f06
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-06-03 22:13:23 +0530

    category

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -8,23 +8,64 @@ export function GET(){
                 {
                     id:1,
                     category:"Biryani",
-                    imgurl:"https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Baguettes%2C_Paris%2C_France_-_panoramio.jpg/1920px-Baguettes%2C_Paris%2C_France_-_panoramio.jpg"
+                    imgurl:"https://sai.madhuram.xyz/wp-content/uploads/2024/06/biriyani_No-bg-e1717429298886.png"
                 },
                 {
                     id:2,
-                    category:"Curry",
-                    imgurl:"https://static.wixstatic.com/media/91e241_475faa4fa56341f3878699dde5ab4904~mv2.jpg/v1/fill/w_864,h_576,al_c,lg_1,q_85,enc_auto/91e241_475faa4fa56341f3878699dde5ab4904~mv2.jpg"
+                    category:"Tiffins",
+                    imgurl:"https://sai.madhuram.xyz/wp-content/uploads/2024/06/dosa_No-bg-e1717429795370.png"
                 },
                 {
                     id:3,
-                    category:"Breads",
-                   imgurl:"https://openthemagazine.com/wp-content/uploads/2015/11/frogfood1.jpg"
+                    category:"Starters",
+                   imgurl:"https://sai.madhuram.xyz/wp-content/uploads/2024/06/Chicken-starter_-No-bg-e1717429367221.png"
                 },
                 {
                     id:4,
                     category:"Chinese",
-                    imgurl:"https://images.herzindagi.info/image/2020/Oct/chole-bhature-calories-one-plate.jpg"
+                    imgurl:"https://sai.madhuram.xyz/wp-content/uploads/2024/06/Noodels-No-bg-e1717429885762.png"
                 },
+                // {
+                //     id:5,
+                //     category:"PaniPuri",
+                //     imgurl:"https://images.herzindagi.info/image/2020/Oct/chole-bhature-calories-one-plate.jpg"
+                // },
+                // {
+                //     id:6,
+                //     category:"Sweets",
+                //     imgurl:"https://images.herzindagi.info/image/2020/Oct/chole-bhature-calories-one-plate.jpg"
+                // },
+                // {
+                //     id:7,
+                //     category:"IceCream",
+                //     imgurl:"https://images.herzindagi.info/image/2020/Oct/chole-bhature-calories-one-plate.jpg"
+                // },
+                // {
+                //     id:8,
+                //     category:"MilkShakes",
+                //     imgurl:"https://images.herzindagi.info/image/2020/Oct/chole-bhature-calories-one-plate.jpg"
+                // },
+                // {
+                //     id:9,
+                //     category:"Pasta",
+                //     imgurl:"https://images.herzindagi.info/image/2020/Oct/chole-bhature-calories-one-plate.jpg"
+                // },
+                // {
+                //     id:10,
+                //     category:"Hot Beverages",
+                //     imgurl:"https://images.herzindagi.info/image/2020/Oct/chole-bhature-calories-one-plate.jpg"
+                // },
+                // {
+                //     id:11,
+                //     category:"Momo's",
+                //     imgurl:"https://images.herzindagi.info/image/2020/Oct/chole-bhature-calories-one-plate.jpg"
+                // },
+                // {
+                //     id:12,
+                //     category:"Sandwiche's",
+                //     imgurl:"https://images.herzindagi.info/image/2020/Oct/chole-bhature-calories-one-plate.jpg"
+                // },
+                // },
             ]
                 
             
