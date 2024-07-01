8816bdc2d2ba830cc5d22368795dc323895c0a46
subhadeepsonu <subhadeepthandaka@gmail.com>
2024-07-01 17:02:34 +0530

    in house cred auth complete

--- app/api/v2/login/route.ts
+++ app/api/v2/login/route.ts
@@ -1,9 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import { loginSchema } from "./schema";
 import bcrypt from "bcrypt"
+import prisma from "@/db";
+import z from "zod"
+import jwt from "jsonwebtoken"
 export async function POST(req:NextRequest){
     try {
-        const data = await req.json()
+        const data:z.infer<typeof loginSchema>  = await req.json()
         const check = loginSchema.safeParse(data)
         if(!check.success){
             return NextResponse.json({
@@ -11,7 +14,32 @@ export async function POST(req:NextRequest){
                 message:"Invalid input"
             })
         }
-        const salt = await bcrypt.genSalt(10)
+        const isOld = await prisma.user.findUnique({
+            where:{
+                email:data.email
+            }
+        })
+        if(isOld){
+            const passcheck = await bcrypt.compare(data.password,isOld.password!)
+            if(passcheck){
+                const token = jwt.sign(isOld,"haha")
+                return NextResponse.json({
+                    success:true,
+                    message:token
+                })
+            }
+            else{
+                return NextResponse.json({
+                    success:false,
+                    message:"Incorrect password"
+                })
+            }
+        }else{
+            return NextResponse.json({
+                success:false,
+                message:"User does not exist"
+            })
+        }
         
     } catch (error) {
         return NextResponse.json({
--- app/api/v2/signup/route.ts
+++ app/api/v2/signup/route.ts
@@ -1,9 +1,12 @@
 import { NextRequest, NextResponse } from "next/server";
 import z from "zod"
+import bcrypt from "bcrypt"
 import { signupSchema } from "./schema";
+import jwt from "jsonwebtoken"
+import prisma from "@/db";
 export async function POST(req:NextRequest){
     try {
-        const data = req.json()
+        const data: z.infer<typeof signupSchema> = await req.json()
         const check = signupSchema.safeParse(data)
         if(!check.success){
             return NextResponse.json({
@@ -11,8 +14,36 @@ export async function POST(req:NextRequest){
                 message:"Inavlid Input"
             })
         }
-        
+        const isOld = await prisma.user.findUnique({
+            where:{
+                email:data.email
+            }
+        })
+        if(isOld){
+            return NextResponse.json({
+                success:false,
+                message:"User already exists"
+            })
+        }
+        const salt = await bcrypt.genSalt(10)
+        const hashed = await bcrypt.hash(data.password,salt)
+        const response = await prisma.user.create({
+            data:{
+                name:data.name,
+                email:data.email,
+                password:hashed
+            }
+        })
+        const token = jwt.sign(response,"haha")
+        return NextResponse.json({
+            success:true,
+            message:response,
+            token:token
+        })
     } catch (error) {
-        
+        return NextResponse.json({
+            success:false,
+            message:`${error}`
+        })
     }
 }
